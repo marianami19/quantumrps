4be1ac8fcade4ef23b014df7e2a1f3c700e98858
marianami19 <nelsonmaria98@gmail.com>
2023-10-03 05:25:00 +0530

    q

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const pool = mysql.createPool({
   host: "bnycrniwlveaoqkdiygk-mysql.services.clever-cloud.com", // If your MySQL server is on the same machine
   user: "u0efstyvunl4hfey", // Replace with your MySQL username (usually "root" for XAMPP)
-  password: "", // Leave it empty if you haven't set a password for your MySQL server in XAMPP
+  password: "LSZH7PTkHUYnnmrhpKAH", // Leave it empty if you haven't set a password for your MySQL server in XAMPP
   database: "bnycrniwlveaoqkdiygk", // Replace with your database name
 });
 
