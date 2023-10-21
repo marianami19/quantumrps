40abf4e86a300eab215a14e363fc03dd9708c33b
marianami19 <45075042+marianami19@users.noreply.github.com>
2023-10-21 20:31:23 +0530

    roof measurement logic backend

--- server/server.js
+++ server/server.js
@@ -156,9 +156,37 @@ app.get('/solar-data', async (req, res) => {
     const solarResponse = await axios.get(
       `https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude=${latitude}&location.longitude=${longitude}&requiredQuality=HIGH&key=${GOOGLE_API_KEY}`
     );
+    const wholeRoofStats = solarResponse.data.solarPotential.wholeRoofStats.areaMeters2
 
+    // Constants for estimation
+    const averageSlopeHeight = 2.5;  // Estimate the average slope height in meters
+    const averageSlopeWidth = 4.0;   // Estimate the average slope width in meters
 
-    res.json({ squareFootArea: solarResponse.data.solarPotential.wholeRoofStats.areaMeters2 });
+    // Sloped Roof Area (Sum of Slope Areas)
+    const slopeSegments = solarResponse.data.solarPotential['roofSegmentStats'];
+    let sumSlopeArea = 0;
+    console.log(slopeSegments.length)
+
+    const x = await slopeSegments.forEach((segment) => {
+      const pitchDegrees = segment.pitchDegrees;
+
+      // Estimating the width and height of the sloped segment
+      const pitchRadians = pitchDegrees * (Math.PI / 180);
+      const halfSlopeHeight = averageSlopeHeight / 2;
+      const halfSlopeWidth = halfSlopeHeight / Math.tan(pitchRadians);
+
+      // Calculate the area based on the estimated width and height
+      const slopeArea = 2 * halfSlopeHeight * halfSlopeWidth;
+
+      sumSlopeArea += slopeArea;
+    });
+
+    // Total Roof Area
+    const totalRoofArea = wholeRoofStats + sumSlopeArea;
+
+    console.log(`Total Roof Area: ${totalRoofArea} square meters`);
+
+    res.json({ squareFootArea: totalRoofArea });
   } catch (error) {
     // console.error('Error fetching solar data:', error);
     res.status(500).json({ error: 'Could not fetch solar data' });
