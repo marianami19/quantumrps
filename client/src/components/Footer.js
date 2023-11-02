c92be3d263298e351d3f017d3c4ed1011059b7a6
marianami19 <45075042+marianami19@users.noreply.github.com>
2023-11-02 13:03:02 +0530

    credit given for photos and website on the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,11 +10,11 @@ const Footer = () => {
           <div className="col-md-4">
             <img src={footer} width="303px" height="182px" ></img>
           </div>
-          
+
           <div className="col-md-2">
-       
+
           </div>
-          
+
           <div className="col-md-2  mt-5  text-start">
             <h4 className="foot-head">CONTACT </h4>
             <p className="foot-desc"> (239) 372-3757</p>
@@ -35,6 +35,20 @@ const Footer = () => {
           </div>
         </div>
       </div>
+      <hr/>
+      <figure className="text-end">
+        <figcaption className="blockquote-footer">
+          Photos by <cite title="https://unsplash.com/photos/brown-and-white-wooden-house-near-green-trees-under-blue-sky-during-daytime-pvdx8c6Y5BY"><a href="https://unsplash.com/@kyddvisuals">Dillon Kydd</a>, <a href="https://unsplash.com/@teapowered">Patrick Robert Doyle
+</a>, <a href ="https://unsplash.com/@ryunosuke_kikuno">Ryunosuke Kikuno</a>, <a href="https://unsplash.com/@juligentx">Julián Gentilezza
+</a>  </cite>on <a href="https://unsplash.com/">Unsplash</a>
+        </figcaption>
+        <figcaption className="blockquote-footer">
+        Website by <cite title="https://www.fiverr.com/s/yNyXPe"><a href="https://www.fiverr.com/s/yNyXPe">Maria Nelson</a></cite>
+
+
+        </figcaption>
+      </figure>
+      
     </footer>
   );
 };
