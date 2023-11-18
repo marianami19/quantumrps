c0b914174ccb6a3b7b598abfe2426f1e871b80ab
marianami19 <45075042+marianami19@users.noreply.github.com>
2023-11-18 14:40:49 +0530

    home page

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "../styles/HeroSection.scss";
 import roof from "../assets/images/roof.jpg";
 
@@ -12,48 +12,50 @@ function HeroSection() {
     }
   };
   return (
-    <div className="container home-screen">
-      <div className="row">
-        <div className="col-md-7">
-          <div className="text-content">
-            <div className="text-group">
-              <div className="text-line">
-                <h1 className="line">Your Roof.</h1>
-                <h1 className="line">Your Rules.</h1>
-                <h1 className="line">Quantum Roofing.</h1>
-              </div>
-              <div className="text-line">
-                <h3 className="description">
-                  At Quantum Roofing and Professional Services, we believe that
-                  your roofing project should be straightforward, stress-free,
-                  and cost-effective.
-                </h3>
-                <p className="description">
-                  Join us in our mission to transform the industry and
-                  experience the difference today.
-                </p>
-              </div>
-              <div className="calculate-button">
-                <button
-                  type="button"
-                  className="btn btn-outline-dark"
-                  onClick={scrollToElement}
-                >
-                  Calculate Your Estimate
-                </button>
+    <Fragment>
+      <div className="img-home d-block d-lg-none">
+        <img src={roof} alt="Roofing" className="center-block" />
+      </div>
+      <div className=" home-screen">
+        <div className="d-flex justify-content-center flex-wrap">
+          <div className="d-flex  flex-column">
+            <div className="text-content">
+              <div className="text-group">
+                <div className="text-line">
+                  <h1 className="line">Your Roof.</h1>
+                  <h1 className="line">Your Rules.</h1>
+                  <h1 className="line">Quantum Roofing.</h1>
+                </div>
+                <div className="text-line">
+                  <h3 className="description">
+                    At Quantum Roofing and Professional Services, we believe<br /> that
+                    your roofing project should be straightforward, stress-<br />free,
+                    and cost-effective.
+                  </h3>
+                  <p className="description">
+                    Join us in our mission to transform the industry and<br />
+                    experience the difference today.
+                  </p>
+                </div>
+                <div className="calculate-button">
+                  <button
+                    type="button"
+                    className="btn btn-outline-dark"
+                    onClick={scrollToElement}
+                  >
+                    Calculate Your Estimate
+                  </button>
+                </div>
               </div>
             </div>
           </div>
+          <div className="img-home d-none d-lg-block">
+            <img src={roof} alt="Roofing" className="center-block" />
+          </div>
         </div>
-        <div className="col-md-5 center-block">
-          {/* <div className="image-container"> */}
-          <img src={roof} alt="Roofing" className="w-100 img-responsive mt-1" />
-          {/* </div> */}
-        </div>
-      </div>
 
-      {/* ... Previous content ... */}
-    </div>
+      </div>
+    </Fragment>
   );
 }
 
