228fb899fbb7f656fd55ab1808f70abaf51af147
marianami19 <45075042+marianami19@users.noreply.github.com>
2023-11-20 15:16:53 +0530

    navbar close on outside click with usRef and useEf

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,25 +1,46 @@
-import React from "react";
-import logo from "../assets/images/logo.png"; // Import your logo image
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from "react";
+import logo from "../assets/images/logo.png";
+import { NavLink } from "react-router-dom";
 import "../styles/Header.scss";
 
 function Header() {
-  const scrollToElement = () => {
-    // Replace 'elementId' with the actual ID of the element you want to scroll to
-    const element = document.getElementById('contact');
+  const [isNavbarOpen, setNavbarOpen] = useState(false);
+  const navbarRef = useRef(null);
 
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+  const handleDocumentClick = (event) => {
+    if (
+      navbarRef.current &&
+      !navbarRef.current.contains(event.target) &&
+      isNavbarOpen
+    ) {
+      setNavbarOpen(false);
     }
   };
 
+  useEffect(() => {
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, [isNavbarOpen]);
+
+  const scrollToElement = () => {
+    const element = document.getElementById("contact");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const toggleNavbar = () => {
+    setNavbarOpen(!isNavbarOpen);
+  };
+
   return (
-    <header className="header sticky-top bg-white" >
+    <header className="header sticky-top bg-white">
       <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container-fluid mx-md-4">
-          {/* <a className="navbar-brand" href="#"> */}
-          <img src={logo} alt="Logo" height={40}  />
-          {/* </a> */}
+        <div className="container-fluid mx-md-4" ref={navbarRef}>
+          <img src={logo} alt="Logo" height={40} />
           <button
             className="navbar-toggler"
             type="button"
@@ -28,26 +49,47 @@ function Header() {
             aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            onClick={toggleNavbar}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div
-            className="collapse navbar-collapse justify-content-end"
+            className={`collapse navbar-collapse justify-content-end ${
+              isNavbarOpen ? "show" : ""
+            }`}
             id="navbarNav"
           >
             <ul className="navbar-nav ml-auto align-items-center">
               <li className="nav-item">
-              <NavLink    className="nav-NavLink " to="/" activeClassName="active">HOME</NavLink >
+                <NavLink
+                  className="nav-NavLink"
+                  to="/"
+                  activeClassName="active"
+                  onClick={toggleNavbar}
+                >
+                  HOME
+                </NavLink>
               </li>
               <li className="nav-item mx-md-4">
-              <NavLink    className="nav-link" to="/about-us" activeClassName="active">ABOUT</NavLink >
+                <NavLink
+                  className="nav-link"
+                  to="/about-us"
+                  activeClassName="active"
+                  onClick={toggleNavbar}
+                >
+                  ABOUT
+                </NavLink>
               </li>
               <li className="nav-item">
                 <div className="head-button text-center ms-2">
-                  <button  onClick={scrollToElement} className="btn btn-outline-dark">CONTACT US</button>
+                  <button
+                    onClick={scrollToElement}
+                    className="btn btn-outline-dark"
+                  >
+                    CONTACT US
+                  </button>
                 </div>
               </li>
-             
             </ul>
           </div>
         </div>
